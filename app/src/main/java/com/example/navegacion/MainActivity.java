517eca2df0ecddab83715116d78bd4b36704fada
Fernando <fcano@elpuig.xeill.net>
2020-10-14 20:24:16 +0200

    --

--- app/src/main/java/com/example/navegacion/MainActivity.java
+++ app/src/main/java/com/example/navegacion/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.navegacion;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
+import android.app.Activity;
 import android.os.Bundle;
 
+import com.example.navegacion.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView((binding = ActivityMainBinding.inflate(getLayoutInflater())).getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+        NavController navController = ((NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment)).getNavController();
+        NavigationUI.setupWithNavController(binding.bottomNavView, navController);
+        NavigationUI.setupWithNavController(binding.toolbar, navController);
     }
 }
